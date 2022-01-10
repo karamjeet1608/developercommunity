9242207db12a9c9cda9069d0b9f6a5eb7fd17c26
karamjeetsingh.coderK <karamjeetsingh.de@gmail.com>
2022-01-10 16:38:43 +0100

    initial project setup
    server end setup
    package installation
    database connection

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
+app.get('/', (req,res)=> res.send('connected to app'))
+
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log(`Server running on port ${port}`));
